8c0a1c8b0c9162c87bbe136ed4e14c50b7fa1a77
Cristobal Dominguez <cristobald@gmail.com>
2022-04-24 21:51:53 +0200

    Updates User Default Form Data

--- db/migrations/hard_reset.sql
+++ db/migrations/hard_reset.sql
@@ -42,7 +42,7 @@ CREATE TABLE wire_transfers(
 );
 
 INSERT INTO users(account, first_name, last_name, email, rut, address, password, balance) 
-    VALUES(1234561, 'Cristóbal', 'Domínguez', 'cristobald@example.com', '12.345.678-5', 'Avda del Mar 1234', 'ClaveSecreta123', 100000),
+    VALUES(1234561, 'Cristóbal', 'Domínguez', 'cristobald@example.com', '12.345.678-5', 'Avda del Mar 1234', '123456', 100000),
           (1234562, 'Pedro', 'Pérez', 'pperez@example.com', '13.932.324-6', 'Tobalaba 9382', 'Password', 100000),
           (1234563, 'Juan', 'Pérez', 'jperez@example.com', '13.932.325-4', 'Tobalaba 9382', 'Passw0rd', 100000),
           (1234564, 'Diego', 'Pérez', 'dperez@example.com', '13.932.326-2', 'Tobalaba 9382', 'P4ssword', 100000);
