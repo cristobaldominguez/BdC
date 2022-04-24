e360147c86b8ac63307b128008620a17978b8200
Cristobal Dominguez <cristobald@gmail.com>
2022-04-24 22:51:47 +0200

    Adds db Config

--- config.js
+++ config.js
@@ -5,9 +5,7 @@ const port = process.env.PORT || 3000
 const root = dirname(fileURLToPath(import.meta.url))
 const db = {
     host: 'localhost',
-    user: 'pg-username',
-    password: 'pg-password',
-    database: 'db-name',
+    database: 'bdc',
     port: 5432
 }
 
